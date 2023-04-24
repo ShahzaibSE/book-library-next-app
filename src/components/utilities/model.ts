7135831e5a276fafd2bb15c3f83f52c47fed2d2d
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-24 23:56:46 +0500

    FEAT: Created a book model for api data.

--- src/components/utilities/model.ts
+++ src/components/utilities/model.ts
@@ -1,3 +1,10 @@
 interface Book {
-    
+    id: number;
+    name: string;
+    type: string;
+    available: boolean;
+    price?: number;
+    currentStock?:number;
+    ibn?:string;
+    author?:string;
 }
