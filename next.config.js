9f873a7aec2459525ca8d65a1bb7ec4f7cbcf4e7
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-25 00:05:12 +0500

    FEAT: Doing some more config for api integeration

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
     appDir: true,
   },
   env:{
-    neon_psql: 'postgres://ShahzaibSE:45XjdAVRPMuF@ep-silent-lab-325030.us-east-2.aws.neon.tech/neondb'
+    neon_psql: 'postgres://ShahzaibSE:45XjdAVRPMuF@ep-silent-lab-325030.us-east-2.aws.neon.tech/neondb',
+    accessToken: "f2b978e7d9445f7981388e37e7633cfb38ea17b0b6db787d8a4bb6df8a79e33a"
   }
 }
 
